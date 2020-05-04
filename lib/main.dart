22e254e2c7fcffa9b0c9a715555f337e0e3d3dd9
Mannydefreitas7 <manny.defreitas7@me.com>
2020-05-04 16:46:13 -0400

    add audio background to info.plist

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:BibleRead/classes/audio/AudioPlayerController.dart';
 import 'package:BibleRead/classes/custom/CustomNavigation.dart';
 import 'package:BibleRead/classes/custom/app_localizations.dart';
@@ -13,11 +15,15 @@ import 'package:BibleRead/views/progress/Progress.dart';
 import 'package:BibleRead/views/settings/Settings.dart';
 import 'package:BibleRead/views/today/Today.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:BibleRead/classes/custom/theme.dart' as Theme;
-
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sqlite_api.dart';
+import 'package:path/path.dart';
 
 Notifications notifications = Notifications();
 
@@ -125,10 +131,38 @@ class _MainAppState extends State<MainApp> {
   void initState() {
     super.initState();
       _initialSetup();
+      getDocuments();
    _firstLaunch = firstLaunch;
    FirstLaunch().isNotFirstLaunch().then((value) => isfirstLaunch =  value);
 
 }
+
+getDocuments() async {
+//   Directory appDocDir = await getApplicationDocumentsDirectory();
+// Stream<FileSystemEntity> appDocPath = appDocDir.list();
+// appDocPath.listen((event) {
+//   print(event);
+// });
+  Database db = await coreData;
+  List chapters = await db.query('ZCHAPTER');
+  chapters.forEach((element) {
+    print(element);
+  });
+ final prefs = await SharedPreferences.getInstance();
+  var keys = prefs.getKeys();
+  print(keys);
+}
+
+ Future<Database> get coreData async {
+    Database _database;
+    final _databaseName = "bibleModel.sqlite";
+    final _databaseVersion = 1;
+    String databasesPath = await getDatabasesPath();
+    String dbPath = join(databasesPath, _databaseName);
+    print(dbPath);
+     _database = await openDatabase(dbPath, version: _databaseVersion); 
+    return _database;
+  }
 
 Future<bool> get firstLaunch => FirstLaunch().isNotFirstLaunch();
 
